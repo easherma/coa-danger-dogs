6440923d043fcccf15a08771f32ac094afd43233
easherma <ericandrewsherman@gmail.com>
2018-08-02 18:07:34 -0500

    kinda less ugly

--- plugins/vue-tables-2.js
+++ plugins/vue-tables-2.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import { ClientTable } from 'vue-tables-2';
 
+
+// const tachyons = function() {
+//     return {
+//         framework:'tachyons',
+//         table:'f6 w-100 center" cellspacing="0"',
+//         row:'pv3 pr3 bb b--black-20',
+//         column:'fw6 bb b--black-20 tl pb3 pr3 bg-white',
+//     }
+// }
 // Vue.use(ClientTable, {}, false);
-Vue.use(ClientTable, {}, false, 'bulma', 'default');
+Vue.use(ClientTable, {}, false, 'bootstrap4', 'footerPagination');
