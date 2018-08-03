7ba7f9bdb0011ed915f0186cc58848e6d7523a71
easherma <ericandrewsherman@gmail.com>
2018-08-02 21:45:39 -0500

    header, footer layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
+    <base-header
+      title="Dangerous Dogs of Austin"
+      subtitle1="A collection of data for your saftey, education, and empowerment"
+      subtitle2="made by Eric Sherman for City of Austin"
+    />
+    <nav class="db dt-l w-100 border-box pa3 ph7-l bg-blue">
+      <Navigation />
+    </nav>
     <nuxt/>
+    <footer class="bg-yellow ph3 ph4-ns pv2 ">
+      <nav class="db dt-l w-100 pb3 tc">
+
+        <Navigation />
+      </nav>
+    </footer>
   </div>
 </template>
 
@@ -9,3 +23,15 @@ html {
 /* using tachyons! */
 }
 </style>
+
+<script>
+import Navigation from '~/components/Navigation.vue'
+import BaseHeader from '~/components/core/BaseHeader'
+
+export default {
+  components: {
+    Navigation,
+    BaseHeader
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,6 @@
 <template>
   <section class="container">
     <div>
-      <base-header
-        title="Dangerous Dogs of Austin"
-        subtitle1="A collection of data for your saftey, education, and empowerment"
-        subtitle2="made by Eric Sherman for City of Austin"
-      />
-
-      <li
-        v-for="(route, index) in $router.options.routes"
-        :key="index"
-      >
-        <nuxt-link :to="route.path">{{ route.name }}</nuxt-link>
-      </li>
       <base-section title="Purpose" />
 
       <div class="fr mr4 mw7 right">
@@ -21,23 +9,12 @@
 
       <base-card-grid />
 
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
-import BaseHeader from '~/components/core/BaseHeader'
 import BaseSection from '~/components/core/BaseSection'
 import BaseCardGrid from '~/components/core/BaseCardGrid'
 import { mapState } from 'vuex'
@@ -48,7 +25,6 @@ import SodaMap from '~/components/SodaMap'
 export default {
   components: {
     AppLogo,
-    BaseHeader,
     BaseSection,
     BaseCardGrid,
     Mapbox,
